aa45b2fa13eb7162b7b14fb01682ce32febc07cc
oskar-krolner <119048718+oskar-krolner@users.noreply.github.com>
2026-05-05 21:58:05 +0200

    Add files via upload

---
+++ global/events.gd
@@ -0,0 +1,17 @@
+extends Node
+
+signal card_aim_started(card_ui: CardUI)
+signal card_aim_ended(card_ui: CardUI)
+signal card_played(card: Card)
+signal card_drag_started(card_ui: CardUI)
+signal card_drag_ended(card_ui: Card)
+signal card_tooltip_requested(card: Card)
+signal tooltip_hide_requested
+
+signal  player_hand_drawn
+signal player_hand_discarded
+signal player_turn_ended
+signal player_died
+
+signal enemy_action_completed(enemy: Enemy)
+signal enemy_turn_ended
